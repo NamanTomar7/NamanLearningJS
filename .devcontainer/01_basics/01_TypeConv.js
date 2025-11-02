69663de07769f738bf2a20719fe8f0d707bbbbf2
Naman Tomar <namantomar776@gmail.com>
2025-11-02 06:04:27 +0000

    updated code

---
+++ .devcontainer/01_basics/01_TypeConv.js
@@ -0,0 +1,68 @@
+// let score="naman"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN
+
+// // "33" => 33
+// // "33abc" => NaN
+// // True => 1
+// // False => 0
+
+// let isLoggedIn = -100
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+// // EmptyString ("") => false
+// // String ("naman") => true
+// // 1 => true
+// // 0 => false
+// // Any negative or positive number => true
+
+// let  someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);;
+
+// ********************************* Operations *****************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "Hello "
+// let str2 = "Naman"
+// console.log(str1+str2);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// // JS starts from left to right 
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 , num2, num3 
+
+// num1 = num2 = num3 = 34+2;
+
+// console.log(num1, num2, num3); 
+// // Instead of this, you can print using table 
+// // clean code:-
+
+// console.table({num1, num2, num3}); // Print table with variable name and values
+// console.table([num1, num2, num3]); // prints table with index numebr and values
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter++); // postfix - will print first and then increase
+// console.log(++gameCounter); // prefix - will increase first and then print
+
+
